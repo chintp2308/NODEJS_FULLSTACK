87e9f448570266db6274f70ae46dfd08fc6362ac
chintp2308 <nphuongchi23@gmail.com>
2024-10-26 10:09:51 +0700

    crud

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,79 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 let getAbout = (req, res) => {
   return res.render("test/about.ejs");
 };
 
-// object: {
-//     key: '' ,
-//     value: ''
-// }
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post CRUD");
+};
+
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  console.log(userId);
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    //check  user data not found
+
+    // let userData;
+    return res.render("editCRUD.ejs", {
+      userUpdate: userData,
+    });
+  } else {
+    return res.send("Users not found! ");
+  }
+};
+
+let putUpdateCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUsers,
+  });
+};
+
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserById(id);
+    return res.send("delete succeed! ");
+  } else {
+    return res.send("User not found! ");
+  }
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAbout: getAbout,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putUpdateCRUD: putUpdateCRUD,
+  deleteCRUD: deleteCRUD,
 };
