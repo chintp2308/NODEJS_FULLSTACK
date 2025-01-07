56d0605240a81aa867eb5460fbf1593f58168d3e
chintp2308 <nphuongchi23@gmail.com>
2025-01-07 23:23:20 +0700

    ScheduleByDate

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -50,9 +50,38 @@ let getDetailDoctorById = async (req, res) => {
     });
   }
 };
+let bulkCreateSchedule = async (req, res) => {
+  try {
+    let infor = await doctorService.bulkCreateScheduleService(req.body);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
+let getScheduleByDate = async (req, res) => {
+  try {
+    let infor = await doctorService.getScheduleByDateService(
+      req.query.doctorId,
+      req.query.date
+    );
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
   postInforDoctor: postInforDoctor,
   getDetailDoctorById: getDetailDoctorById,
+  bulkCreateSchedule: bulkCreateSchedule,
+  getScheduleByDate: getScheduleByDate,
 };
